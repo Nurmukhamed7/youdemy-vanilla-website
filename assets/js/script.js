18626c0fdeb7ed03ddf2a084f21e7512fc4da69d
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-02 19:58:33 +0100

    NAVBAR TOGGLER FOR MOBILE

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,18 @@ window.addEventListener('load', function () {
 	circle.style.animation = 'none'
 	document.body.classList.add('loaded')
 })
+
+/**
+ * NAVBAR TOGGLER FOR MOBILE
+ */
+const navbar = document.querySelector('[data-navbar]')
+const navTogglers = document.querySelectorAll('[data-nav-toggler]')
+const overlay = document.querySelector('[data-overlay]')
+
+const toggleNavbar = function () {
+	navbar.classList.toggle('active')
+	overlay.classList.toggle('active')
+	document.body.classList.toggle('nav-active')
+}
+
+addEventOnElements(navTogglers, 'click', toggleNavbar)
