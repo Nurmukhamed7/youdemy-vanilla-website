1dccd5d505a13d8b24e8986583d2c73c61fd14ed
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2024-01-02 16:16:25 +0100

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * add eventListener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+	for (let i = 0, len = elements.length; i < len; i++) {
+		elements[i].addEventListener(eventType, callback)
+	}
+}
+
+/**
+ * PRELOADER
+ */
